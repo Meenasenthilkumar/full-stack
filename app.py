eb757b68fa3467077765a966783e71eb363676ae
meena <meeena.s16@gmail.com>
2026-09-10 17:55:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+1.Built in function
+
+print
+len --> word --> 4
+type 
+input
+
+1.type conversion -->
+int("123")-->123
+str(123) -->"123"
+float("12.35") -->12.5
+bool(0) --> false
+
+2.math & length 
+
+len
+[5,8,2] --> 8
+max([1,8,9]) -->9
+min([1,6,10]) -->1
+sum([1,2,3]) -->6
+round(4.6) -->5
+
+3.list 
+sorted([3,1,2]) --> [1,2,3]
+reversed([1,2,3])
+
+2.user-defined function
+
+user defined function means that you create,not given by python
+
+reusable 
+
+syntax:
+def functionname():
+    print()
+
+def add():
+    print("hello")
+
+def add(a,b):
+    return a+b
+
+3.lambda function:
+a lambda function is an single line function defined without a name using lambda keyword.
+
+
+add =lambda a,b:a+b
+print(add(5,3))
+
+syntax
+lambda arguments:expression
+
+
+
+
+
+
